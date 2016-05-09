df58b9bcc65dde973e705bf16eb2f2efaa2d273b
Camille Nibungco <camille.nibungco@gmail.com>
2016-05-09 00:40:00 -0700

    functional!

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
  
 import { Tasks } from '../api/tasks.js';
 
-//import './task.js';
+import './stuff.js';
 import './body.html';
  
 Template.body.helpers({
